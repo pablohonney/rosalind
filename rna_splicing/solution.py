c1e15ee18c3ec46f3e12b45950d5bc10f9d348e8
arquero <pabloarquerro@gmail.com>
2022-03-28 13:31:13 +0100

    add parse_fasta_records

--- rna_splicing/solution.py
+++ rna_splicing/solution.py
@@ -16,16 +16,7 @@ class ParsedInput:
     introns: List[str]
 
 
-def parse_raw_input(text: str) -> ParsedInput:
-    lines = text.split()
-    records = []
-    for i in range(0, len(lines) - 1, 2):
-        records.append((lines[i], lines[i + 1]))
-
-    return ParsedInput(dna=records[0][1], introns=[r[1] for r in records[1:]])
-
-
-def solve(dna: str, introns: list, translation_table: dict):
+def solve(dna: str, introns: list, translation_table: dict) -> str:
     spliced_dna = splice_dna(dna, introns)
     rna = transcribe(spliced_dna)
     protein = translate(translation_table, rna)
