78a9906bef4ed40556ebc6a3430104066f36a087
arquero <pabloarquerro@gmail.com>
2022-03-29 17:05:13 +0100

    update iinferring mrna from protein test

--- inferring_mrna_from_protein/solution.py
+++ inferring_mrna_from_protein/solution.py
@@ -1,20 +1,17 @@
 from collections import Counter
 
 from translating_rna_into_protein.solution import TRANSLATION_TABLE
-from utils import STOP_CODONS
 
 
 def count_amino_acid_codons(table: dict) -> dict:
-    counter = Counter(table.values())
-    counter.pop("Stop")
-    return counter
+    return Counter(table.values())
 
 
 amino_acid_codons_counts = count_amino_acid_codons(TRANSLATION_TABLE)
 
 
 def infer_mrna_from_protein(protein: str, modulus: int = 10**6) -> int:
-    count = len(STOP_CODONS)
+    count = amino_acid_codons_counts['Stop']
     for amino_acid in protein:
         count *= amino_acid_codons_counts[amino_acid]
 
