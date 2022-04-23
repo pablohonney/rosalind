81d01aedcf33a5d0350c9b8ddac580f80ddb3aac
arquero <pabloarquerro@gmail.com>
2022-04-23 12:51:29 +0100

    add mortal fibonacci tests

--- mortal_fibonacci_rabbits/tests.py
+++ mortal_fibonacci_rabbits/tests.py
@@ -3,3 +3,7 @@ from .solution import RabbitIncubator
 
 def test_mortal_rabbits():
     assert RabbitIncubator(months=6, age=3).breed() == 4
+
+
+def test_exercise():
+    assert RabbitIncubator(months=100, age=18).breed() == 351932180421200619432
